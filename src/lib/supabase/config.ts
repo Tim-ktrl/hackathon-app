358d7482395be95a34d75dfc43c806ce5ed13186
Timothy Kijjambu <kijjambut14@gmail.com>
2026-09-12 17:57:38 -0700

    Finalize Codebase X-Ray MVP

--- src/lib/supabase/config.ts
+++
@@ -1,5 +0,0 @@
-export function supabaseConfig() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const key = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-  return url && key ? { url, key } : null;
-}
